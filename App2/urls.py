adc447077837b8c98b1cc0de55301b890052f8ef
Srikanth <dasarisrikanth1111@gmail.com>
2026-09-05 13:02:32 +0530

    Removing the App2 Files

--- App2/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('hom1',views.hom1,name="home1")
-]
--- App2/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def home1(request):
-    return HttpResponse("Hello this is App2 Content")
